f800f74cf9b7c3b4af23e40676d9ca0f3d041904
Turkey5 <turkteq@gmail.com>
2025-12-09 12:25:03 -0600

    Add startup message to main method

--- midiMarimba.java
+++ midiMarimba.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class midiMarimba {
   public static void main(String[] args) {
-      
+      System.out.println("startup");
   }
   public static void startup() {
     
